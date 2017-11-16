22c8ba27ab08ed53a787df751e4c9f058226efd2
Parth Patel <pat@odoo.com>
2017-11-16 15:59:41 +0530

    [IMP] crm|web|mail: Onbarding Improvements - Add missing tour stage in crm creating opportunity and minor fixes - Label, Placeholder and Tip IMP - Lost & Won filter IMP - Restrict Create new sales channel - Adding Help on ProgressBar - No Content Help Improvements - Schedule Activity Button Layout Change - Chatter Log IMP

--- addons/crm/models/crm_lead.py
+++ addons/crm/models/crm_lead.py
@@ -909,14 +909,16 @@ class Lead(models.Model):
     @api.model
     def get_empty_list_help(self, help):
         if help:
-            alias_record = self.env.ref("crm.mail_alias_lead_info", raise_if_not_found=False)
-            if alias_record and alias_record.alias_domain and alias_record.alias_name:
-                email = '%s@%s' % (alias_record.alias_name, alias_record.alias_domain)
-                email_link = "<a href='mailto:%s'>%s</a>" % (email, email)
-                dynamic_help = _("""All email incoming to %s will automatically
-                    create new opportunity. Update your business card, phone book, social media,...
-                    Send an email right now and see it here.""") % (email_link,)
-                return '<p class="oe_view_nocontent_create">%s</p>%s<p>%s</p>' % (_('Click to add a new opportunity'), help, dynamic_help)
+            if self._context.get('default_type') == 'lead':
+                alias_record = self.env.ref("crm.mail_alias_lead_info", raise_if_not_found=False)
+                help_title = ""
+                if alias_record and alias_record.alias_domain and alias_record.alias_name:
+                    email = '%s@%s' % (alias_record.alias_name, alias_record.alias_domain)
+                    email_link = "<a href='mailto:%s'>%s</a>" % (email, email)
+                    help_title = _('Create a new lead or send an email to %s') % (email_link)
+            else:
+                help_title = _('Create a new opportunity to add it to your pipeline')
+            return '<p class="oe_view_nocontent_create">%s</p><p>%s</p>' % (help_title, help)
         return super(Lead, self.with_context(
             empty_list_help_model='crm.team',
             empty_list_help_id=self._context.get('default_team_id', False),
