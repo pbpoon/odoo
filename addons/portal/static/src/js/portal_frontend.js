a8b59ca0e70408945083b282f10c848b2100ae04
kap-odoo <kap@odoo.com>
2017-12-06 15:28:30 +0530

    [IMP]account:change in calculation of due days.

--- addons/portal/static/src/js/portal_frontend.js
+++ addons/portal/static/src/js/portal_frontend.js
@@ -3,10 +3,25 @@ odoo.define('o_has_portal_frontend.o_has_portal_frontend', function (require) {
 
 require('web.dom_ready');
 var config = require('web.config');
+var time = require('web.time');
 
 if(!$('.o_has_portal_frontend').length) {
     return $.Deferred().reject("DOM doesn't contain '.o_has_portal_frontend'");
 }
+
+$('timeago.timeago').each(function(index, el){
+        var datetime = $(el).attr('datetime'),
+            datetime_obj = time.str_to_date(datetime),
+            // if presentation 7 days, 24 hours, 60 min, 60 second, 1000 millis old(one week)
+            // then return fix formate string else timeago
+            display_str = "";
+        if (datetime_obj && datetime_obj.getTime() - new Date().getTime() > 365 * 24 * 60 * 60 * 1000) {
+            display_str = datetime_obj.toDateString();
+        } else {
+            display_str = moment(datetime_obj).fromNow();
+        }
+        $(el).text(display_str);
+})
 
 var $bs_sidebar = $(".o_has_portal_frontend .bs-sidebar");
     $(window).on('resize', _.throttle(adapt_sidebar_position, 200, {leading: false}));
