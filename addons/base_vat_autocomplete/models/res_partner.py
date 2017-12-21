e79506c9da1ed4de28555f6371265c3c17760d72
Olivier Colson <oco@odoo.com>
2017-12-21 15:15:33 +0100

    [IMP] base_vat: connect directly to VIES without using a library, so that we can set a custom timeout for the connection.
    [IMP] base_vat_autocomplete: use the method defined in base_vat to connect to VIES instead of duplicating the code.

--- addons/base_vat_autocomplete/models/res_partner.py
+++ addons/base_vat_autocomplete/models/res_partner.py
@@ -42,19 +42,15 @@ class ResPartner(models.Model):
         for partner in self:
             if not partner.vat:
                 return {}
-            if len(partner.vat) > 5 and partner.vat[:2].lower() in stdnum_vat.country_codes:
-                # Equivalent to stdnum_vat.check_vies(partner.vat).
-                # However, we want to add a custom timeout to the suds.client
-                # because by default, it's 120 seconds and this is to long.
+            if len(partner.vat) > 5 and partner.vat[:2].lower() in stdnum_vat.country_codes: #TODO OCO les country codes font qu'on ne peut pas s'en passer, semble-t-il ... mais bon => vatnumber n'a pas ça ? Qu'on n'en garde qu'un seul des deux ... => de toute façon, ce check est-il réellement primordial ? > tester
                 try:
-                    client = Client(stdnum_vat.vies_wsdl, timeout=5)
-                    partner_vat = stdnum_vat.compact(partner.vat)
-                    result = client.service.checkVat(partner_vat[:2], partner_vat[2:])
+                    partner_vat = stdnum_vat.compact(partner.vat) #TODO OCO voir si recoder ça permettrait de retirer la dépendance
+                    result = partner.vies_vat_check(partner_vat[:2], partner_vat[2:], except_to_simple_check=False)
                 except:
                     # Avoid blocking the client when the service is unreachable/unavailable
                     return {}
 
-                if not result['valid']:
+                if not result:
                     return {}
 
                 if (not partner.name) and (result['name'] != '---'):
