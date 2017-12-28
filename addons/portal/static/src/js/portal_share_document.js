66bf1bf4a3464eb2ff4ca7e7e35040cd3932f0d0
Barad Mahendra <mba@openerp.com>
2017-12-28 12:15:33 +0530

    [IMP]Improve the js code

--- addons/portal/static/src/js/portal_share_document.js
+++ addons/portal/static/src/js/portal_share_document.js
@@ -40,13 +40,10 @@ var ProtalShareDoc = Widget.extend({
                     var self = this;
                     this._active_view = null;
                     this._view_manager = null;
-                    this.getSession().user_has_group('base.group_erp_manager').then(
-                        function(has_group) {
-                        if (has_group) {
-                            if (widget && widget.action && widget.action.context.share_icon) {
-                                self.share_action = widget.action.context.share_action ? widget.action.context.share_action : 'portal.mail_share_document_action';
-                                self.$el.removeClass('o_hidden');
-                            }
+                    this.getSession().user_has_group('base.group_erp_manager').then(function (has_group) {
+                        if (has_group && widget && widget.action && widget.action.context.share_icon) {
+                            self.share_action = widget.action.context.share_action ? widget.action.context.share_action : 'portal.mail_share_document_action';
+                            self.$el.removeClass('o_hidden');
                         }
                     });
                     break;
