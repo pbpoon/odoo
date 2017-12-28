f45fc5bd44cc6f426ec6c05c35a9b6581c2d5025
Mitali Patel <mpa@odoo.com>
2017-12-28 12:58:26 +0530

    [IMP] mail: add validation for email

--- addons/mail/wizard/invite_user.py
+++ addons/mail/wizard/invite_user.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, fields, models
+from odoo import api, fields, models, tools, _
+from odoo.exceptions import ValidationError
 
 
 class Invite(models.TransientModel):
@@ -15,4 +16,7 @@ class Invite(models.TransientModel):
     def invite_new_user(self):
         """Process new email addresses : create new users """
         invite_emails = [email for email in self.email.split('\n')]
-        return self.env['res.users'].web_dashboard_create_users(invite_emails)
+        is_valid_emails = all([tools.email_re.findall(email) for email in invite_emails])
+        if is_valid_emails:
+            return self.env['res.users'].web_dashboard_create_users(invite_emails)
+        raise ValidationError(_('Invalid Email! Please enter a valid email address.'))
