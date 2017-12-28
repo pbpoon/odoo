bcc4741b5cb3d46e2b93e6561dd4ca22e14411a3
RomainLibert <rli@odoo.com>
2017-12-28 11:30:57 +0100

    [IMP] hr.holidays split
    Model hr.holidays has been split into leave.request and
    leave.allocation. A new report has been added in order to aggregate
    datas from both allocations and requests. The views have been modified
    accordingly.

--- addons/project_timesheet_holidays/__init__.py
+++ addons/project_timesheet_holidays/__init__.py
@@ -12,7 +12,7 @@ def post_init(cr, registry):
     from odoo import api, SUPERUSER_ID
 
     env = api.Environment(cr, SUPERUSER_ID, {})
-    for leave_type in env['hr.holidays.status'].search([('timesheet_generate', '=', True), ('timesheet_project_id', '=', False)]):
+    for leave_type in env['leave.type'].search([('timesheet_generate', '=', True), ('timesheet_project_id', '=', False)]):
         company = leave_type.company_id or env.user.company_id
         leave_type.write({
             'timesheet_project_id': company.leave_timesheet_project_id.id,
