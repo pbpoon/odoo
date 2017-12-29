d388a6cc94fb6594da209c1a5c5f3d8c5cc0119e
Richard Mathot <rim@odoo.com>
2017-12-29 16:46:13 +0530

    [FIX] test_mail: adapt to new module order
        Since website is not a dependency to survey anymore, order of install of
        the modules may change.

--- addons/test_mail/tests/test_performance.py
+++ addons/test_mail/tests/test_performance.py
@@ -139,7 +139,7 @@ class TestAdvMailPerformance(TransactionCase):
             'default_res_model': 'mail.test.activity',
         })
 
-        with self.assertQueryCount(admin=47, emp=52):  # com runbot 46 - 51 // test_mail only: 35 - 40
+        with self.assertQueryCount(admin=49, emp=54):  # com runbot 48 - 53 // test_mail only: 35 - 40
             model.create({
                 'summary': 'Test Activity',
                 'res_id': record.id,
