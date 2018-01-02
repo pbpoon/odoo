de483c76ce2bb4c5b9cdc88e2adc9ea57619d47b
Ravi Gohil <rgo@odoo.com>
2018-01-02 16:13:47 +0530

    [IMP] account_check_printing, hr_expense_check: made field storing check numbers character instead of integer,
    Integer field for check numbers shows check numbers as amount(i.e., with thousand separator) on UI, which is wrong, hence replaced it with character type field and a constraint to allow only numbers to be stored in it.

--- addons/account_check_printing/models/account_payment.py
+++ addons/account_check_printing/models/account_payment.py
@@ -11,7 +11,7 @@ class AccountRegisterPayments(models.TransientModel):
     check_amount_in_words = fields.Char(string="Amount in Words")
     check_manual_sequencing = fields.Boolean(related='journal_id.check_manual_sequencing')
     # Note: a check_number == 0 means that it will be attributed when the check is printed
-    check_number = fields.Integer(string="Check Number", readonly=True, copy=False, default=0,
+    check_number = fields.Char(string="Check Number", readonly=True, copy=False, default=0,
         help="Number of the check corresponding to this payment. If your pre-printed check are not already numbered, "
              "you can manage the numbering in the journal configuration page.")
 
@@ -43,7 +43,7 @@ class AccountPayment(models.Model):
 
     check_amount_in_words = fields.Char(string="Amount in Words")
     check_manual_sequencing = fields.Boolean(related='journal_id.check_manual_sequencing')
-    check_number = fields.Integer(string="Check Number", readonly=True, copy=False,
+    check_number = fields.Char(string="Check Number", readonly=True, copy=False,
         help="The selected journal is configured to print check numbers. If your pre-printed check paper already has numbers "
              "or if the current numbering is wrong, you can change it in the journal configuration page.")
 
@@ -91,10 +91,14 @@ class AccountPayment(models.Model):
         if not self[0].journal_id.check_manual_sequencing:
             # The wizard asks for the number printed on the first pre-printed check
             # so payments are attributed the number of the check the'll be printed on.
-            last_printed_check = self.search([
-                ('journal_id', '=', self[0].journal_id.id),
-                ('check_number', '!=', 0)], order="check_number desc", limit=1)
-            next_check_number = last_printed_check and last_printed_check.check_number + 1 or 1
+
+            # Currently, framework does not support field type casting in search(), hence converted ORM search to query
+            sql_query = """ SELECT check_number::int FROM account_payment
+                WHERE journal_id = %s AND check_number::int <> 0
+                ORDER BY check_number::int DESC LIMIT 1 """
+            self._cr.execute(sql_query, (self[0].journal_id.id,))
+            last_printed_check_number = self._cr.fetchone()
+            next_check_number = last_printed_check_number and last_printed_check_number[0] + 1 or 1
             return {
                 'name': _('Print Pre-numbered Checks'),
                 'type': 'ir.actions.act_window',
