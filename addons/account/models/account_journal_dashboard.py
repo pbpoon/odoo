22f41cf1c9fb8ab415de0967097a5382ee8323a2
Tejas Shahu <tsh@odoo.com>
2018-01-08 18:47:51 +0530

    [IMP] Account: Configuration Steps(If Fiscal Year Date has been selected from configuration steps then display those entries only which is coming after that fiscal_year_date, otherwise display all the entries)
    [IMP] Account: Code optimization

--- addons/account/models/account_journal_dashboard.py
+++ addons/account/models/account_journal_dashboard.py
@@ -156,20 +156,35 @@ class account_journal(models.Model):
             last_bank_stmt = self.env['account.bank.statement'].search([('journal_id', 'in', self.ids)], order="date desc, id desc", limit=1)
             last_balance = last_bank_stmt and last_bank_stmt[0].balance_end or 0
             #Get the number of items to reconcile for that bank journal
-            self.env.cr.execute("""SELECT COUNT(DISTINCT(line.id))
-                            FROM account_bank_statement_line AS line
-                            LEFT JOIN account_bank_statement AS st
-                            ON line.statement_id = st.id
-                            WHERE st.journal_id IN %s AND st.state = 'open' AND line.amount != 0.0
-                            AND not exists (select 1 from account_move_line aml where aml.statement_line_id = line.id)
-                        """, (tuple(self.ids),))
+            date_opening = datetime.strftime(fields.date.today(), '%Y-%m-%d')
+            if self.company_id.account_opening_date:
+                date_opening = self.company_id.account_opening_date
+
+            query = "SELECT COUNT(DISTINCT(line.id)) FROM account_bank_statement_line AS line \
+                    LEFT JOIN account_bank_statement AS st \
+                    ON line.statement_id = st.id WHERE st.journal_id IN %s AND st.state = 'open' \
+                    AND line.amount != 0.0 \
+                    AND not exists (select 1 from account_move_line aml \
+                    where aml.statement_line_id = line.id)"
+
+            if self.company_id.account_opening_date:
+                query = query + ' AND line.date >= %s'
+                self.env.cr.execute(query, (tuple(self.ids), date_opening))
+            else:
+                self.env.cr.execute(query, (tuple(self.ids), ))
+
             number_to_reconcile = self.env.cr.fetchone()[0]
             # optimization to read sum of balance from account_move_line
             account_ids = tuple(ac for ac in [self.default_debit_account_id.id, self.default_credit_account_id.id] if ac)
             if account_ids:
                 amount_field = 'balance' if (not self.currency_id or self.currency_id == self.company_id.currency_id) else 'amount_currency'
-                query = """SELECT sum(%s) FROM account_move_line WHERE account_id in %%s AND date <= %%s;""" % (amount_field,)
-                self.env.cr.execute(query, (account_ids, fields.Date.today(),))
+                move_line_query = "SELECT sum(%s) FROM account_move_line WHERE account_id in %s" % (amount_field, account_ids)
+
+                if self.company_id.account_opening_date:
+                    move_line_query += ' AND date >= %s'
+                else:
+                    move_line_query += ' AND date <= %s'
+                self.env.cr.execute(move_line_query, (date_opening, ))
                 query_results = self.env.cr.dictfetchall()
                 if query_results and query_results[0].get('sum') != None:
                     account_sum = query_results[0].get('sum')
@@ -287,7 +302,11 @@ class account_journal(models.Model):
     def action_open_reconcile(self):
         if self.type in ['bank', 'cash']:
             # Open reconciliation view for bank statements belonging to this journal
-            bank_stmt = self.env['account.bank.statement'].search([('journal_id', 'in', self.ids)])
+            # Domain for fiscal_year_date selection
+            domain = [('journal_id', 'in', self.ids)]
+            if self.company_id.account_opening_date:
+                domain.append(('date', '>=', self.company_id.account_opening_date))
+            bank_stmt = self.env['account.bank.statement'].search(domain)
             return {
                 'type': 'ir.actions.client',
                 'tag': 'bank_statement_reconciliation_view',
